3633b677aac442f00ce9bb9234182855df5dc1d7
Sanish <108924686+SanishBasukala@users.noreply.github.com>
2023-03-04 09:25:19 +0545

    create Locked levels

--- Assets/Scripts/Menuscript.cs
+++ Assets/Scripts/Menuscript.cs
@@ -1,13 +1,50 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menuscript : MonoBehaviour
 {
-    Scene currentScene;
-
-    private void Update()
+    string currentScene;
+    public int levelPassed;
+    public Button level2Button, level3Button, level4Button, level5Button;
+    private void Start()
     {
-        currentScene = SceneManager.GetActiveScene();
+        level2Button.interactable = false;
+        level3Button.interactable = false;
+        level4Button.interactable = false;
+        level5Button.interactable = false;
+
+        switch (levelPassed)
+        {
+            case 1:
+                level2Button.interactable = true;
+                level2Button.transform.Find("Locked").gameObject.SetActive(false);
+                break;
+            case 2:
+                level2Button.interactable = true;
+                level2Button.transform.Find("Locked").gameObject.SetActive(false);
+                level3Button.interactable = true;
+                level3Button.transform.Find("Locked").gameObject.SetActive(false);
+                break;
+            case 3:
+                level2Button.interactable = true;
+                level2Button.transform.Find("Locked").gameObject.SetActive(false);
+                level3Button.interactable = true;
+                level3Button.transform.Find("Locked").gameObject.SetActive(false);
+                level4Button.interactable = true;
+                level4Button.transform.Find("Locked").gameObject.SetActive(false);
+                break;
+            case 4:
+                level2Button.interactable = true;
+                level2Button.transform.Find("Locked").gameObject.SetActive(false);
+                level3Button.interactable = true;
+                level3Button.transform.Find("Locked").gameObject.SetActive(false);
+                level4Button.interactable = true;
+                level4Button.transform.Find("Locked").gameObject.SetActive(false);
+                level5Button.interactable = true;
+                level5Button.transform.Find("Locked").gameObject.SetActive(false);
+                break;
+        }
     }
     public void GetMainScene()
     {
@@ -19,7 +56,32 @@ public class Menuscript : MonoBehaviour
     }
     public void GetLevel1Scene()
     {
+        currentScene = "Level1";
         SceneManager.LoadScene("Level1");
+    }
+    public void GetLevel2Scene()
+    {
+        levelPassed = 1;
+        currentScene = "Level2";
+        SceneManager.LoadScene("Level2");
+    }
+    public void GetLevel3Scene()
+    {
+        levelPassed = 2;
+        currentScene = "Level3";
+        SceneManager.LoadScene("Level3");
+    }
+    public void GetLevel4Scene()
+    {
+        levelPassed = 3;
+        currentScene = "Level4";
+        SceneManager.LoadScene("Level4");
+    }
+    public void GetLevel5Scene()
+    {
+        levelPassed = 4;
+        currentScene = "Level5";
+        SceneManager.LoadScene("Level5");
     }
     public void GetCreateScene()
     {
@@ -31,9 +93,27 @@ public class Menuscript : MonoBehaviour
     }
     public void ChangeScene()
     {
-        if (currentScene == SceneManager.GetSceneByName("Level1"))
+        if (currentScene == "Level1")
         {
-            //SceneManager.LoadScene("Level2");
+            GetLevel2Scene();
+        }
+        if (currentScene == "Level2")
+        {
+            GetLevel3Scene();
+        }
+        if (currentScene == "Level3")
+        {
+            GetLevel4Scene();
+        }
+        if (currentScene == "Level4")
+        {
+            GetLevel5Scene();
+        }
+        if (currentScene == "Level5")
+        {
+            //Roll end credits
+
+            Debug.Log("Game finished");
         }
     }
     public void Exitgame()
